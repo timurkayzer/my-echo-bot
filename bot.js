9c3b58073dda3ac3c5ea4ad551725ed4c946ba56
Timur Kaiser <timurkayzer@gmail.com>
2018-08-21 01:37:49 +0500

    first commit to release

---
+++ bot.js
@@ -0,0 +1,34 @@
+const telegraf = require('telegraf')
+
+const key = '671670157:AAGsg6zep2t_ec5hNn7WSL-ZKr9860_lTZA'
+
+const bot = new telegraf(key)
+
+bot.on('message',ctx=>{
+    ctx.reply(JSON.stringify(ctx.update , null , 4))
+})
+
+bot.on('location',ctx=>{
+    ctx.reply(JSON.stringify(ctx.update , null , 4))
+})
+
+bot.on('contact',ctx=>{
+    ctx.reply(JSON.stringify(ctx.update , null , 4))
+})
+
+bot.on('image',ctx=>{
+    ctx.reply(JSON.stringify(ctx.update , null , 4))
+})
+
+bot.on('file',ctx=>{
+    ctx.reply(JSON.stringify(ctx.update , null , 4))
+})
+
+const PORT = process.env.PORT || 3000;
+const URL = 'https://kaiserechobot.herokuapp.com/';
+
+bot.webhookReply = false;
+bot.telegram.setWebhook(`${URL}/bot`).catch((err) => console.log(err));
+
+// Start https webhook
+bot.startWebhook('/bot', null, PORT)
